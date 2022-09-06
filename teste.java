5da19c6977772962382a2b2876aa59dcd690f676
andriuszzz <andre.genolima@gmail.com>
2022-09-06 20:40:22 -0300

    commit

---
+++ teste.java
@@ -0,0 +1,27 @@
+public class teste {
+
+	public static void main(String[] args) {
+
+		String product1 = "Computer";
+		String product2 = "Office desk";
+
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+
+        System.out.println("products: ");
+        System.out.println("Computer, whith price is $ " + price1);
+        System.out.println("office desk, whith price is $ " + price2);
+        System.out.println("record: " + age + " years old, code " + code + " and gender: " + gender);
+        System.out.println("measue with eight decimal places: " + measure);
+        System.out.println("Roudes (three decimal places): " + measure);
+        System.out.println();
+		
+}
+
+
+}
